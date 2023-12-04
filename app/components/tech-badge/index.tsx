76280cda46079f032219eade72ed9331a1db0115
Bruno Lima <brunolimadevelopment@gmail.com>
2023-12-04 16:09:44 -0300

    feat: homepage

--- app/components/tech-badge/index.tsx
+++ app/components/tech-badge/index.tsx
@@ -4,8 +4,8 @@ type TechBadgeProps = {
 
 export const TechBadge = ({ name }: TechBadgeProps) => {
     return (
-        <span className="text-emerald-400 bg-emerald-900/80 text-sm py-1 px-3 rounded-lg">
+        <div className="text-emerald-400 bg-emerald-900/80 text-sm py-1 px-3 rounded-lg">
             {name}
-        </span>
+        </div>
     )
 }
