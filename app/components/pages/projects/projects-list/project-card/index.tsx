44d1f8d733aa1f9c4b9216d12f7214f894f5759a
Bruno Lima <brunolimadevelopment@gmail.com>
2023-12-06 23:30:52 -0300

    feat: projects single page

--- app/components/pages/projects/projects-list/project-card/index.tsx
+++ app/components/pages/projects/projects-list/project-card/index.tsx
@@ -2,20 +2,22 @@ import Image from "next/image"
 
 export const ProjectCard = () => {
     return (
-        <div>
-            <div>
+        <div className="group rounded-lg h-[436px] flex flex-col bg-gray-800 overflow-hidden border-2 border-gray-800 hover:border-emerald-500 opacity-70 hover:opacity-100 transition-all">
+            <div className="w-full h-48 overflow-hidden">
                 <Image
                     width={380}
                     height={200}
                     src="https://media.graphassets.com/FRhUdgUQTHmLmwf9u0BA"
                     alt="Thumbnail do projeto BookWise"
                     unoptimized
+                    className="w-full h-full object-cover group-hover:scale-110 duration-500 transition-all"
                 />
             </div>
 
-            <div>
-                <strong>BookWise</strong>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. hic!</p>
+            <div className="flex-1 flex flex-col p-8">
+                <strong className="font-medium text-gray-50/90 group-hover:text-emerald-500 transition-all">BookWise</strong>
+                <p className="mt-2 text-gray-400 line-clamp-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. hic!</p>
+                <span className="text-gray-300 text-sm font-medium block mt-auto truncate">Next.js, Next Auth, Stitches, Radix, </span>
             </div>
         </div>
     )
