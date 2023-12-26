f973a4344246eb98adec4d5b77fb79f707103b2d
Bruno Lima <brunolimadevelopment@gmail.com>
2023-12-26 00:29:43 -0300

    feat: formulario integração webhook discord

--- app/components/contact-form/index.tsx
+++ app/components/contact-form/index.tsx
@@ -6,6 +6,8 @@ import { SectionTitle } from "../section-title"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
+import axios from "axios"
+import toast from "react-hot-toast"
 
 
 const contactFormSchema = z.object({
@@ -17,11 +19,18 @@ const contactFormSchema = z.object({
 type ContactFormData = z.infer<typeof contactFormSchema>
 
 export const ContactForm = () => {
-    const { handleSubmit, register } = useForm<ContactFormData>({
+    const { handleSubmit, register, reset, formState: { isSubmitting } } = useForm<ContactFormData>({
         resolver: zodResolver(contactFormSchema)
     })
-    const onSubmit = (data: ContactFormData) => {
-        console.log(data)
+    const onSubmit = async (data: ContactFormData) => {
+        try {
+            await axios.post('/api/contact', data)
+            toast.success('Mensagem enviada com sucesso!')
+            reset()
+        } catch {
+            alert('erro')
+            toast.error('Ocorreu um erro ao enviar a mensagem. Tente novamente!')
+        }
     }
     return (
         <section id="contact" className="py-16 px-6 md:py-32 flex items-center justify-center bg-gray-950">
@@ -32,7 +41,7 @@ export const ContactForm = () => {
                     <input {...register('name')} type="text" placeholder="Nome" className="w-full h-14 bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-2 ring-emerald-600" />
                     <input {...register('email')} type="email" placeholder="Email" className="w-full h-14 bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-2 ring-emerald-600" />
                     <textarea {...register('message')} placeholder="Mensagem" maxLength={500} className="resize-none w-full h-[138px] bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-2 ring-emerald-600" />
-                    <Button className="w-max mx-auto mt-6 shadow-button">
+                    <Button className="w-max mx-auto mt-6 shadow-button" disabled={isSubmitting}>
                         Enviar mensagem
                         <HiArrowNarrowRight size={18} />
                     </Button>
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Header } from './components/header'
 import { ContactForm } from './components/contact-form'
 import { Footer } from './components/footer'
 import { BackToTop } from './components/back-to-top'
+import { Toaster } from './components/toaster'
 
 
 export const metadata = {
@@ -34,6 +35,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR" className={`${inter.variable} ${plexMono.variable}`}>
       <body>
+        <Toaster />
         <Header />
         <BackToTop />
         {children}
