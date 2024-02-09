66dc7e561f844eb30c1795ed223d142d9344a646
Bruno Lima <brunolimadevelopment@gmail.com>
2024-02-09 15:01:21 -0300

    feat: add new project

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ const getPageData = async (): Promise<HomePageData> => {
           url
           iconSvg
         }
-        knownTechs {
+        knownTechs(first: 100) {
           iconSvg
           name
           startDate
@@ -63,7 +63,7 @@ const getPageData = async (): Promise<HomePageData> => {
   `
   return fetchHygraphQuery(
     query,
-    60 * 60 * 24 // a query fica cacheada por 24h
+    60 * 60 * 24// a query fica cacheada por 24h
   )
 }
 export default async function Home() {
